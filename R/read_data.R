9103c8e52be0e947706dbb7faf4a9e96e74f91b0
humb1rd <eva.muecke@gmail.com>
2017-08-25 16:19:53 -0700

    updates

--- R/read_data.R
+++ R/read_data.R
@@ -1,8 +1,12 @@
-#’read data in ‘file_name’ as a data frame
+#’ read data in ‘file_name’ as a data frame
 #'
+#' here we describe what read_data does
+#' @title read_data
 #' @param file_name name of the file that contains file name
 #' @return a data frame with bike counts
 #' @export
 read_data <-function(file_name) {
-  # read data in ‘file_name’ as a data frame
+  print(paste(file_name, "is ready to be read into R"))
+  my_df<-readxl::read_excel(file_name)
+  return(my_df)
 }
